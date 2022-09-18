3db9275d4409af71da7414952274fd4c6e6cf60d
Pareekshith <93633322+Pareekshith123@users.noreply.github.com>
2022-09-18 19:08:20 +0530

    initial commit
    this keyword example program

---
+++ Thiskeyword.java
@@ -0,0 +1,19 @@
+package first_project;
+
+public class Thiskeyword {
+
+	int c;
+	public  Thiskeyword(int c){
+		this.c=c;
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		Thiskeyword vc=new Thiskeyword(5);
+		System.out.println("the value of this c is ="+vc.c);
+	}
+
+}
