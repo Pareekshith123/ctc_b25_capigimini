bae1a3f7f0f8fffbddebffcf6fed25769feb4e33
Pareekshith <93633322+Pareekshith123@users.noreply.github.com>
2022-09-28 21:54:47 +0530

    initial commit

---
+++ trycatch1.java
@@ -0,0 +1,17 @@
+package first_project;
+
+public class trycatch1 {
+
+	public static void main(String[] args) {
+		try {
+			int d=0;
+			int s=45/d;
+			System.out.println("will not be printed");
+		}catch(ArithmeticException e){
+			System.out.println("divide with the zero is invalid ");
+			
+		}}}
+
+	
+
+
---
+++ twodarray.java
@@ -0,0 +1,28 @@
+package first_project;
+
+import java.util.*;
+public class twodarray {
+
+	public static void main(String[] args) {
+		int [][] p= {{1,2,3,4},{12,34,56,}};
+		
+		/*for(int[] s:p){
+			for(int d:s){
+			System.out.println(d);
+			}
+		}*/
+		for(int i=0;i<=p.length;i++) {
+			for(int j=0;j<=p.length;j++) 
+			{
+				System.out.println(p[i][j]);
+			}
+		}
+
+		
+			}
+		}
+		
+		
+	
+
+
